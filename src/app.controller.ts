82564fbc1ff5ea347d517c61dad973964ee13270
JakabGabor95 <trexgab648@gmail.com>
2023-04-23 17:24:21 +0200

    create controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//prefix ('/events') or ({path: '/events'})
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/bye')
+  getBye(): string {
+    return 'bye';
   }
 }
