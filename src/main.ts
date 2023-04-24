9a1a37f61e3190a3611881cdc5948431b388bd6a
JakabGabor95 <trexgab648@gmail.com>
2023-04-24 21:30:33 +0200

    specify logger messages

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    //I can specify what logger messages what I want
+    //so after that basic 'green' messages disappear
+    logger: ['error', 'warn', 'debug'],
+  });
   //if I use validation pipe globally, I don't need to use in controller @Body
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
